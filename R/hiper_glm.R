8434b98bf3cec759ea9c0ef609d1cd99cc884af1
thepianistalex <llang7@jh.edu>
2024-02-02 15:34:21 -0500

    Roxygenize my package
    Roxygenize the package
    
    Add return to the hiper_glm roxygen

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -1,4 +1,11 @@
-hiper_glm <- function(desgin, outcome){
+#' Title the core fitting function for hiperglm package
+#'
+#' @param design design matrix
+#' @param outcome outcome vector
+#'
+#' @export
+#' @return an object of hiperres
+hiper_glm <- function(design, outcome){
   result <- list()
   class(result) <- "hiperres"
   return(result)
--- R/result_analysis.R
+++ R/result_analysis.R
@@ -1,11 +1,29 @@
+#' Title coef method for hiperres object
+#'
+#' @param result the hiperres object
+#' @param ... further arguments passed to or from other methods
+#'
+#' @export
 coef.hiperres <- function(result, ...){
   warning("This function is not implemented yet")
 }
 
+#' Title vcov method for hiperres object
+#'
+#' @param result the hiperres object
+#' @param ... further arguments passed to or from other methods
+#'
+#' @export
 vcov.hiperres <- function(result, ...){
   warning("This function is not implemented yet")
 }
 
+#' Title print method for hiperres object
+#'
+#' @param result the hiperres object
+#' @param ... further arguments passed to or from other methods
+#'
+#' @export
 print.hiperres <- function(result, ...){
   print("This function is not implemented yet")
 }

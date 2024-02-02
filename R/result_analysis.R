629941df9b25636b11ef2d99ed295857cbd1293c
thepianistalex <llang7@jh.edu>
2024-02-02 00:11:04 -0500

    add hiperres objects methods

---
+++ R/result_analysis.R
@@ -0,0 +1,11 @@
+coef.hiperres <- function(result, ...){
+  warning("This function is not implemented yet")
+}
+
+vcov.hiperres <- function(result, ...){
+  warning("This function is not implemented yet")
+}
+
+print.hiperres <- function(result, ...){
+  print("This function is not implemented yet")
+}

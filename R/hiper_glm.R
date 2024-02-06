ab41d17aa32d7f4a3f3f2aae997fa32cbe3c4c4b
thepianistalex <llang7@jh.edu>
2024-02-06 14:23:35 -0500

    adding "model" argument to hiper_glm()

--- R/hiper_glm.R
+++ R/hiper_glm.R
@@ -2,11 +2,16 @@
 #'
 #' @param design design matrix
 #' @param outcome outcome vector
+#' @param model model to fit
 #'
 #' @export
 #' @return an object of hiperres
-hiper_glm <- function(design, outcome){
+hiper_glm <- function(design, outcome, model){
   # To do list: implement model fitting
+  supported_models <- c("linear", "logit")
+  if(!(model %in% supported_models)){
+    stop(sprintf("Model %s is not supported", model))
+  }
   warning("This function is not implemented yet")
   hglm_out <- list()
   class(hglm_out) <- "hglm"

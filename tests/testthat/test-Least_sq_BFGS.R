2f6c8f81f500a1e0dd344e62266f13cf4450c6a8
thepianistalex <llang7@jh.edu>
2024-02-06 18:49:18 -0500

    update test for Least sq vs BFGS

--- tests/testthat/test-Least_sq_BFGS.R
+++ tests/testthat/test-Least_sq_BFGS.R
@@ -1,11 +1,12 @@
 test_that("BFGS vs least_sq for linear model", {
-  n_obs <- 100; n_pred <- 5
+  n_obs <- 100
+  n_pred <- 5
   data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 721)
   design <- data$design
   outcome <- data$outcome
 
-  least_sq_out <- hiper_glm(design, outcome, model = 'linear', mle_finder = "least_sq")
-  bfgs_out <- hiper_glm(design, outcome, model = 'linear', mle_finder = "BFGS")
+  least_sq_out <- hiper_glm(design, outcome, model = 'linear', option = list(mle_solver = 'least_sq'))
+  bfgs_out <- hiper_glm(design, outcome, model = 'linear', option = list(mle_solver = 'BFGS'))
 
   expect_true(are_all_close(coef(least_sq_out), coef(bfgs_out),
                             abs_tol = 1e-2, rel_tol = 1e-2))

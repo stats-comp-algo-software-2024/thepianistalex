67d4005f89e756627961a441b7c944236ed9c4a7
thepianistalex <llang7@jh.edu>
2024-02-17 17:19:19 -0500

    implement and test the grad/loglik func for logit

--- tests/testthat/test-Gradient_analytics_vs_numerical.R
+++ tests/testthat/test-Gradient_analytics_vs_numerical.R
@@ -12,3 +12,18 @@ test_that("numerical vs analytics gradient for linear model", {
 
   expect_true(are_all_close(gradient_analytical, gradient_numerical))
 })
+
+test_that("numerical vs analytics gradient for logit model", {
+  n_obs <- 100
+  n_pred <- 5
+  model <- 'logit'
+  data <- simulate_data(n_obs, n_pred, model = model, seed = 721)
+  design <- data$design
+  outcome <- data$outcome
+  beta <- data$coef_true
+
+  gradient_analytical <- compute_logit_gradient(beta, design, outcome)
+  gradient_numerical <- compute_numerical_grad(compute_logit_log_likelihood, beta, design, outcome)
+
+  expect_true(are_all_close(gradient_analytical, gradient_numerical))
+})

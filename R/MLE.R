c39e1a7c5350d819dbedd737a1473fa31ad8a807
thepianistalex <llang7@jh.edu>
2024-02-20 18:24:53 -0500

    Add statistical convergence check to newton

--- R/MLE.R
+++ R/MLE.R
@@ -9,19 +9,35 @@ find_mle_linear <- function(design, outcome) {
 
 #' Newton’s method solver
 #' @noRd
-find_mle_newton <- function(design, outcome, beta_init = NULL, max_iter = 1000) {
+find_mle_newton <- function(design, outcome, beta_init = NULL, max_iter = 1000, chisq_tol = 0.01) {
   p <- ncol(design)
   if(is.null(beta_init)) {
     beta_init <- rep(0, p)
   }
-  beta <- beta_init
+  beta_old <- beta_init
+  threshold <- 0.5*qchisq(chisq_tol,1)
+  converged <- FALSE
   for(i in 1:max_iter) {
-    gradient <- compute_logit_gradient(beta, design, outcome)
-    hessian <- compute_logit_heassian(beta, design, outcome)
-    beta <- beta + solve(hessian, gradient)
+
+    gradient <- compute_logit_gradient(beta_old, design, outcome)
+    hessian <- compute_logit_heassian(beta_old, design, outcome)
+    beta_new <- beta_old + solve(hessian, gradient)
+
+    log_lik_old <- compute_logit_log_likelihood(beta_old, design, outcome)
+    log_lik_new <- compute_logit_log_likelihood(beta_new, design, outcome)
+    if(abs(log_lik_new - log_lik_old) < threshold) {
+      converged <- TRUE
+      break
+    } else{
+      beta_old <- beta_new
+    }
   }
 
-  return(beta)
+  if(!converged) {
+    warning(paste("Newton’s method did not converge after", max_iter, "iterations"))
+  }
+
+  return(beta_new)
 }
 
 #' BFGS solver
